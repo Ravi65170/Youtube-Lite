db19b487ae61b471fe705fac6fe620fa93ecce59
Ravi Prakash Yadav <Ravi65710@gmail.com>
2023-08-15 17:53:14 +0530

    Fixed build failed vercel

--- src/components/SearchReasultVideoCard.jsx
+++ src/components/SearchReasultVideoCard.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { abbreviateNumber } from "js-abbreviation-number";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-
-import VideoLength from "../shared/videoLength";
+import VideoLength from "../shared/VideoLength";
 
 const SearchReasultVideoCard = ({ video }) => {
   return (
